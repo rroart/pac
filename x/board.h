c2229ed05a3198284da9365105e4c92192782ede
Roar Thronæs <roart@nvg.ntnu.no>
1999-03-02 14:42:41 +0000

    not needed: boards[][][]

--- x/board.h
+++ x/board.h
@@ -23,7 +23,7 @@ static int foods=168;	//how many food items there are on each board
 
 #define initen {0}
 
-static char boards[LEVELS][BOARDHEIGHT+1][BOARDWIDTH+1];
+//static char boards[][][];
 
 class DynamicElement;
 class G_Board; 
